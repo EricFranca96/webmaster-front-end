3df0a117146e46254573363268739bee93f77aed
Eric França <ericprogramacao96@gmail.com>
2023-05-31 19:56:06 -0300

    Funções

---
+++ javaScript/aulasPraticas/functions.js
@@ -0,0 +1,27 @@
+window.onload = function () {
+
+    /*function teste(num1, num2) {
+        return num1 + num2;
+    }
+
+    var variavel = teste(10, 20);
+
+    console.log(variavel);
+   */
+
+   /*var obj = {'nome':'Guilherme', 'idade':23};
+
+    obj.func = function(){
+        console.log('minha funcao');
+    }
+
+    obj.func()*/
+
+    var variavel = [];
+
+    variavel[0] = [];
+
+    variavel[0][2] = 'Guilherme';
+
+    console.log(variavel[0][2]);
+}
