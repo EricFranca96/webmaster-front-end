eaea8e323d2bac51ced89fa7c8cdde167e57b5e4
Eric França <ericprogramacao96@gmail.com>
2023-06-05 16:05:39 -0300

    Iniciando com JQuery e eventos de carregamento

--- javaScript/aulasPraticas/functions.js
+++
@@ -1,9 +0,0 @@
-var nome = "Guilherme";
-switch (nome) {
-    case "João":
-        //Posso fazer a ação que eu quiser.
-    break;
-    case "Ghilherme":
-        console.log("A condição bateu!");
-    break;
-}
---
+++ jquery/js/functions.js
@@ -0,0 +1,19 @@
+/*
+$(function(){
+    alert("Olá Mundo!");
+})
+
+$(document).ready(function() {
+    alert("Olá Mundo!");
+});
+
+$(window).on('load', function() {
+    alert("Olá Mundo!");
+})
+*/
+
+//Modo de trabalhar para facilitar a leitura.
+
+$(function() {
+    alert("Olá Mundo!");
+})
