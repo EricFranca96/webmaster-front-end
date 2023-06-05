54bb5671619211abfe27a69e12b531aba3a5bda6
Eric França <ericprogramacao96@gmail.com>
2023-06-05 16:51:01 -0300

    Introduzindo os eventos

--- jquery/js/functions.js
+++ jquery/js/functions.js
@@ -1,6 +1,34 @@
-$(function() {
-    
-    $('.artigo1 > p').css('color','rgba(0,0,0,0.6');
-    console.log($('.artigo1 > p').css('color'));
+$(function () {
 
-})
+
+    function validarCliqueHover() {
+        $('.artigo1').click(function () {
+            $('.artigo2').css('background-color', 'purple');
+        });
+
+        $('.artigo1').hover(function () {
+            $('.artigo2').css('background-color', 'red');
+        }, function () {
+            $('.artigo2').css('background-color', 'rgb(100,100,100)');
+        });
+    }
+
+    function validarFormulario() {
+        $('select').change(function () {
+            console.log("meu select foi alterado!");
+        });
+    }
+    validarFormulario();
+    validarCliqueHover();
+
+
+    /*
+        $('textarea').focus(function() {
+            alert('Estou dentro do textarea');
+        })
+    
+        $('textarea').blur(function() {
+            alert('Estou fora do textarea!');
+        });
+    */
+});
