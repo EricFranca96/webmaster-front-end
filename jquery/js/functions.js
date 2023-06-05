7af05e63e305082a7b1fd3c0d8f0a34c659d131f
Eric França <ericprogramacao96@gmail.com>
2023-06-05 20:12:42 -0300

    Manipulando dimensões dinamicamente

--- jquery/js/functions.js
+++ jquery/js/functions.js
@@ -1,14 +1,18 @@
 $(function(){
 
-    var el = $('div.box');
-
-    el.css('background-color','green');
-
-    function teste(){
-        el.css('color','red');
-    }
-    teste();
 
 
 
+    //var elWidht = $('.box').width();
+
+    //Pegamos a largura do nosso elemento.
+    $('.box').width();
+
+    //Setamos a largura.
+    $('.box').width('900');
+
+    console.log("Width: "+$('.box').width());
+    console.log("Inner Width: "+$('.box').innerWidth());
+    console.log("Outer Width: "+$('.box').outerWidth());
+    //Para altura é a mesma logica acima.
 });
