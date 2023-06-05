d40c773e0ae9b038edbb01f24bb8f5ad103d379e
Eric França <ericprogramacao96@gmail.com>
2023-06-05 10:13:43 -0300

    Switch e case

--- javaScript/aulasPraticas/functions.js
+++ javaScript/aulasPraticas/functions.js
@@ -1,17 +1,9 @@
-var n = 0;
-//continue
-//break
-/*for(var i = 0; i <= 10; i++){
-    if(i == 2){
-        break;
-    }
-    console.log(i);
-}*/
-while(n < 10){
-    if(n == 5){
-        break;
-    }
-    console.log(n);
-
-    n++;
+var nome = "Guilherme";
+switch (nome) {
+    case "João":
+        //Posso fazer a ação que eu quiser.
+    break;
+    case "Ghilherme":
+        console.log("A condição bateu!");
+    break;
 }
