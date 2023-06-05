36fa4253b3306ff213dbe63d6253a063eff5de4b
Eric França <ericprogramacao96@gmail.com>
2023-06-05 10:30:43 -0300

    Criando loja no JavaScript

---
+++ javaScript/miniLojaVirtual/script.js
@@ -0,0 +1,23 @@
+const items = [
+    {
+        id: 0,
+        nome: 'camiseta',
+        img: 'image.jpg',
+        quantidade: 0
+    },
+    {
+        id: 0,
+        nome: 'camiseta',
+        img: 'image.jpg',
+        quantidade: 0
+    },
+
+    {
+        id: 0,
+        nome: 'camiseta',
+        img: 'image.jpg',
+        quantidade: 
+    },
+
+
+]
