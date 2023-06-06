bf9124f42eb74f7430dbac1d49c99a8e029835f6
Eric França <ericprogramacao96@gmail.com>
2023-06-06 09:04:28 -0300

    Manipulando a posição no DOM

--- jquery/js/functions.js
+++ jquery/js/functions.js
@@ -1,42 +1,8 @@
 $(function(){
+    //Função append() para adicionar conteúdo/elementos no final do elemento que selecionarmos.
+    //$('.box div').eq(1).append('<h3>Meu elemento adicionado dinamicamente</h3>');
 
-    $('input[type=button]').click(function(){
-        var str = $('input[type=text]').val();
-        //Split separa nossa string com base no delimitador.
-        // var var2 = v.split("@");
-        // console.log(var2);
+    var el = $('<h3>Meu Conteúdo</h3>').appendTo($('.box'));
 
-        //Substr recorta nossa string
-        //console.log(str.substr(1,4));
-
-
-        var splitstr = str.split("@");
-        if(splitstr[1] == 'hotmail.com'){
-            $('input[type=text]').css('opacity','0');
-        }else{
-            console.log("A condição não bateu!");
-        }
-    })
-
-
-
-
-
-
-
-
-
-    /*
-    var el = $('.box');
-
-    el.html('<div class="teste"<h1> Meu elemento!</h1></div>');
-    el.html('<h1 class="texto1">Meu texto via Javascript!</h1>');
-
-    $('.box2').text("<div></div>");
-    $('.box2').text($('.box2').text() + "Ola mundo!");
-
-    $('input[type=text]').val("Olá mundo!");
-
-    $('textarea').text("Olá mundo");
-    */
+    el.css('color','red');
 });
