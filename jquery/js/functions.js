bb7da254c7f02c536880a1453b21ecaa1f4f1f05
Eric França <ericprogramacao96@gmail.com>
2023-06-06 08:37:28 -0300

    Manipulando textos dinamicamente

--- jquery/js/functions.js
+++ jquery/js/functions.js
@@ -1,18 +1,42 @@
 $(function(){
 
+    $('input[type=button]').click(function(){
+        var str = $('input[type=text]').val();
+        //Split separa nossa string com base no delimitador.
+        // var var2 = v.split("@");
+        // console.log(var2);
+
+        //Substr recorta nossa string
+        //console.log(str.substr(1,4));
+
+
+        var splitstr = str.split("@");
+        if(splitstr[1] == 'hotmail.com'){
+            $('input[type=text]').css('opacity','0');
+        }else{
+            console.log("A condição não bateu!");
+        }
+    })
 
 
 
-    //var elWidht = $('.box').width();
 
-    //Pegamos a largura do nosso elemento.
-    $('.box').width();
 
-    //Setamos a largura.
-    $('.box').width('900');
 
-    console.log("Width: "+$('.box').width());
-    console.log("Inner Width: "+$('.box').innerWidth());
-    console.log("Outer Width: "+$('.box').outerWidth());
-    //Para altura é a mesma logica acima.
+
+
+
+    /*
+    var el = $('.box');
+
+    el.html('<div class="teste"<h1> Meu elemento!</h1></div>');
+    el.html('<h1 class="texto1">Meu texto via Javascript!</h1>');
+
+    $('.box2').text("<div></div>");
+    $('.box2').text($('.box2').text() + "Ola mundo!");
+
+    $('input[type=text]').val("Olá mundo!");
+
+    $('textarea').text("Olá mundo");
+    */
 });
