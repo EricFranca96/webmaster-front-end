311e63d07d14d891c6cd51e61ba2d518d77ac84c
Eric França <ericprogramacao96@gmail.com>
2023-06-07 20:07:16 -0300

    Timeout

--- jquery/js/functions.js
+++ jquery/js/functions.js
@@ -1,10 +1,21 @@
 $(function () {
 
+    var timeOut = function(){
+        $('.box2').animate({
+            'width' : '40%',
+            'height' : '500px',
+            'marginLeft' : '100px',
+            'marginTop' : '200px'
+        },2000);
+    }
+
     $('.box1').animate({
         'width' : '40%',
         'height' : '500px',
+        'marginLeft' : '100px',
+        'paddingTop' : '200px'
     },2000, function () {
-        console.log("Terminamos nossa animação.");
+        setTimeout(timeOut,3000);
     });
 
 
