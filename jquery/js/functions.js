c8cab16c5e0088870e9e8073a8b45fe87040e52d
Eric França <ericprogramacao96@gmail.com>
2023-06-13 09:11:23 -0300

    O que é ajax

--- jquery/js/functions.js
+++ jquery/js/functions.js
@@ -1,7 +1,11 @@
 $(function () {
 
-    var el = $('#source .texto').clone();
-    
-    $('#source .texto').appendTo('#container');
+    $.ajax({
+        'url':'conteudo.html',
+        //'method':'post',
+        //data:{'nome':'guilherme','idade':'23'}
+    }).done(function (data) {
+        $('#container').append(data);
+    });
 
 });
