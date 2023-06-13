a604a79f4c2c189421cf4f03d382888e15062e0c
Eric França <ericprogramacao96@gmail.com>
2023-06-13 08:57:56 -0300

    Clonar e mover elementos

--- jquery/js/functions.js
+++ jquery/js/functions.js
@@ -1,13 +1,7 @@
 $(function () {
 
-    $('.box1').addClass('minhaclasse');
-
-    //$('.minhaClasse').remove();
-
-    $('.box1').removeClass('minhaclasse');
-
-    var el = $('.box1').find('.child1').find('.child2');
-
-    el.css('color','green');
+    var el = $('#source .texto').clone();
+    
+    $('#source .texto').appendTo('#container');
 
 });
