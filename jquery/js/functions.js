2fc5ca773925a0f86a7964294d443970f7dd3124
Eric França <ericprogramacao96@gmail.com>
2023-06-20 17:03:29 -0300

    Finalizando validação

--- jquery/js/functions.js
+++ jquery/js/functions.js
@@ -1,17 +1,6 @@
 $(function () {
 
-    /*
-    var frase = 'eric@hotmail.com';
-
-    var verifica = frase.match(/^(.*?)@(.*?)$/);
-
-    if(verifica != null){
-        console.log("Encontramos algo!");
-        console.log(verifica);
-    }else{
-        console.log("Não encontramos nada");
-    }
-    */
+    //Funções de abrir e fechar formulário
 
     abrirJanela();
     verificarCliqueFechar();
@@ -36,36 +25,92 @@ $(function () {
 
     }
 
+    //Eventos do formulário
 
-    $('form#form1').submit(function () {
+    $('input[type=text]').focus(function () {
+        resetarCampoInvalido($(this));
+    });
 
+
+    $('form#form1').submit(function (e) {
+        e.preventDefault();
         var nome = $('input[name="nome"]').val();
         var telefone = $('input[name="telefone"]').val();
         var email = $('input[name="email"]').val();
 
-        //Contando a quantidade de espaços e os respectivos valores.
-        var amount = nome.split(' ').length;
-        var splitStr = nome.split(' ');
-        if (amount >= 2) {
-            for (var i = 0; i < amount; i++) {
-                if (splitStr[i].match(/[^[A-Z]{1}[a-z]{1,}$]/)) {
-                    console.log("Nossa condição bateu, podemos dar continuidade");
-                } else {
-                    aplicarCampoInvalido($('input[name=nome]'));
-                    return false;
-                }
-            }
-        } else {
-            aplicarCampoInvalido($('input[name=nome]'));
+        if (verificarNome(nome) == false) {
+            aplicarCampoInvalido($('input[name="nome"]'));
             return false;
-        }
-
-        function aplicarCampoInvalido(el){
-            el.css('border', '2px solid red');
-            el.data('invalido', true);
+        } else if (verificarTelefone(telefone) == false) {
+            aplicarCampoInvalido($('input[name="telefone"]'));
+            return false;
+        } else if (verificarEmail(email) == false) {
+            aplicarCampoInvalido($('input[name="email"]'));
+            return false;
+        } else {
+            alert("Formulário enviado com sucesso!");
         }
 
         //Se chegou até o final é por que está tudo okay!
     })
+
+    //Funções para estilizar o campo do formulário
+
+    function aplicarCampoInvalido(el) {
+        // el.css('color', 'red');
+        el.css('border', '2px solid red');
+        el.val('Campo invalido!');
+        //el.data('invalido', true);
+    }
+
+    function resetarCampoInvalido(el) {
+        // el.css('color', '#ccc');
+        el.css('border', '1px solid #ccc');
+        el.val('');
+    }
+
+    //Funções para verificar nossos campos
+
+    function verificarNome(nome) {
+        //Contando a quantidade de espaços e os respectivos valores.
+        if (nome == '') {
+            return false;
+        }
+        var amount = nome.split(' ').length;
+        var splitStr = nome.split(' ');
+        if (amount >= 2) {
+            for (var i = 0; i < amount; i++) {
+                if (splitStr[i].match(/^[A-Z]{1}[a-z]{1,}$/)) {
+
+                } else {
+                    return false;
+                }
+            }
+        } else {
+
+            return false;
+        }
+    }
+
+    function verificarTelefone(telefone) {
+        if (telefone == '') {
+            return false;
+        }
+
+        if (telefone.match(/^\([0-9]{2}\)[0-9]{4}-[0-9]{4}$/) == null) {
+            return false;
+        }
+
+    }
+
+    function verificarEmail(email) {
+        if (email == '') {
+            return false;
+        }
+        if (email.match(/^[a-z]{1,}@[a-z]{1,}$/) == null) {
+            return false;
+        }
+    }
+
 
 });
